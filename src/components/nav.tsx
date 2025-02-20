27c08e15d853e47d1e86bdcc9a3c3c2a8290d052
IRABARUTA <willynorbert53@gmail.com>
2025-02-20 10:43:05 +0200

    first commit

---
+++ src/components/nav.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+  
+
+const Nav = () => {
+  return (
+    <div>
+         <button className="px-2 py-4 bg-blue-500 bg-primary text-white rounded-md hover:bg-black-600">
+      Click Me
+    </button>
+    </div>
+  );
+};
+export default Nav;
